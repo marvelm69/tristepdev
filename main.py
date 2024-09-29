e885c5332c6b0e4f31105aa88184f3ad5256cfa1
Marvel Martawidjaja <100202616+marvelm69@users.noreply.github.com>
2024-09-29 23:21:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -351,7 +351,7 @@ def main():
         course_spreadsheet_id = st.secrets["google_sheets"]["spreadsheet_id"]
         job_spreadsheet_id = st.secrets["google_sheets_job"]["spreadsheet_id"]
         online_jobs_spreadsheet_id = st.secrets["google_sheets_job"]["online_jobs_spreadsheet_id"]
-        online_jobs_spreadsheet_id = "1yoBVx63C_zR_wHO-KPiOu9BTCqgtQyr1tHorBGQnmP4"
+        online_jobs_spreadsheet_id = "1OYeOlipgcnGnRrEqHisVCvjF28ukiEr-5ktk8BoTmzk"
         
         page = st.sidebar.selectbox("Select Page", ["Manage Courses", "Manage Jobs"])
         
